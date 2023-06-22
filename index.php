b6b4f8fa9a0d1adb55588a6aa0408689b4858a5c
Braga Elena-Isabella <student6@academydevopsoutlook.onmicrosoft.com>
2023-06-22 14:55:20 +0300

    navbar update & login with navbar

--- index.php
+++ index.php
@@ -63,7 +63,6 @@
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                           <li><a class="dropdown-item" href="login.php"><i class="fa fa-user-tie fa-fw"></i>Log in</a></li>
-                          <li><a class="dropdown-item" href="profile.html"><i class="fa fa-user-tie fa-fw"></i> Profile</a></li>
                           <li><a class="dropdown-item" href="account.html"><i class="fas fa-sliders-h fa-fw"></i> Account</a></li>
                           <li><a class="dropdown-item" href="settings.html"><i class="fas fa-cog fa-fw"></i> Settings</a></li>
                           <li><hr class="dropdown-divider"></li>
--- login.php
+++ login.php
@@ -35,15 +35,84 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="index.html"><img style="width:5rem;" src="assets/images/logo.png"></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 profile-menu"> 
+                    <li class="nav-item px-3">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item px-3">
+                        <a class="nav-link" href="about-us.html">Despre noi</a>
+                      </li>
+                      <li class="nav-item dropdown px-3">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Cursurile noastre
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="curs1.html">Front-End Developer</a></li>
+                          <li><a class="dropdown-item" href="curs2.html">Back-End Developer</a></li>
+                          <li><a class="dropdown-item" href="curs3.html">C#</a></li>
+                          <li><a class="dropdown-item" href="curs4.html">Java</a></li>
+                          <li><a class="dropdown-item" href="curs5.html">Python</a></li>
+                          <li><a class="dropdown-item" href="curs6.html">iOS Developer</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="toate-cursurile.html">Toate cursurile</a></li>
+                        </ul>
+                      </li>
+                      <li class="nav-item dropdown px-3">
+                        <a class="nav-link" href="our-team.html">Echipa noastra</a>
+                      </li>
+                      <li class="nav-item dropdown px-3">
+                        <a class="nav-link" href="become-teacher.html">Devino profesor!</a>
+                      </li>
+                      <li class="nav-item dropdown px-3">
+                        <a class="nav-link" href="become-student.html">Devino student!</a>
+                      </li>
+                      <li class="nav-item dropdown px-3">
+                        <a class="nav-link" href="contact.html">Contact</a>
+                      </li>
+                      <li class="nav-item dropdown px-2">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          <i class="fas fa-user"></i>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="login.php"><i class="fa fa-user-tie fa-fw"></i>Log in</a></li>
+                          <li><a class="dropdown-item" href="account.html"><i class="fas fa-sliders-h fa-fw"></i> Account</a></li>
+                          <li><a class="dropdown-item" href="settings.html"><i class="fas fa-cog fa-fw"></i> Settings</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="logout.php"><i class="fas fa-sign-out-alt fa-fw"></i> Log Out</a></li>
+                        </ul>
+                      </li>
+               </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
   <title>Log in</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <title>Proiect de licenta</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/style.css'>
+    <script src='assets/js/app.js'></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-<div class="card text-center" style="padding:20px;">
-  <h3>Log in</h3>
+<div class="card text-center" style="padding:20px; padding-top:5vh">
+  <h3 class="pt-5">Log in</h3>
 </div><br>
 <div class="container">
   <div class="row">

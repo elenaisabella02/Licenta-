45874baf1757c62bb00f4175e14692b942fd08ed
DanCro <dancro1122@gmail.com>
2023-06-22 22:22:21 +0300

    contact script

--- yourform-processor.php
+++ yourform-processor.php
@@ -23,8 +23,7 @@ $email_body = "You have received a new message from the user $name. \n".
 "email adress: $visitor_email\n".
 "Here is the message: \n $message \n";
 
-// $to = "bragaelenaisabella@yahoo.com";
-$to = "dancro1122@gmail.com";
+$to = "bragaelenaisabella@yahoo.com";
 $headers = "From: $email_from \r\n";
 mail($to,$email_subject,$email_body,$headers);
 
